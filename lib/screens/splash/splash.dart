92afdb9502f109c1f544bbeb195cf0de3250088c
Hyzinx <hyzin633@gmail.com>
2026-02-16 09:51:02 +0200

    Add splashScreen

--- lib/screens/splash/splash.dart
+++ lib/screens/splash/splash.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Splash extends StatelessWidget {
   const Splash({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Expanded(
+        child: Container(
+          color: Colors.black,
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset("assets/img/splash.png"),
+                SizedBox(height: 40),
+                Text(
+                  "Dooit",
+                  style: GoogleFonts.inter(color: Colors.white, fontSize: 50),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
